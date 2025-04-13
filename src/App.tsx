4cd1caf975a71472c9c85cde628ee976aa2110bc
sunkam96 <sunkam.sai@gmail.com>
2025-04-12 23:21:47 -0400

    add empty services route without provider

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Services bookingData={bookingData} setBookingData={setBookingData}/>} />
         <Route path="/:providerId" element={<Services bookingData={bookingData} setBookingData={setBookingData}/>} />
+        <Route path="/services" element={<Services bookingData={bookingData} setBookingData={setBookingData} />} />
         <Route path="/services/:providerId" element={<Services bookingData={bookingData} setBookingData={setBookingData} />} />
         <Route path="/availability" element={<Availability bookingData={bookingData} setBookingData={setBookingData} />} />
         <Route path="/confirm" element={<Confirm bookingData={bookingData} setBookingData={setBookingData}/>} />
