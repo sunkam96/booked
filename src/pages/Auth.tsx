b424c44b7fa8cd8e2410560d49d54c07d01fe3d9
sunkam96 <sunkam.sai@gmail.com>
2025-08-28 10:10:58 -0400

    move api url to .env

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,19 +1,20 @@
 import '../App.css';
-import {useLocation} from "react-router";
+import { useLocation } from "react-router";
 
-function Auth(){
+function Auth() {
     const location = useLocation();
     const { providerId } = location.state || {};
+    const googleAuthUrl = process.env.GOOGLE_API_URL;
 
     return (
         <div className="page-frame">
             <div className="page-container">
                 <h2>Hello {providerId}</h2>
                 <h3>Please grant permission to your calendar so appointments may be scheduled</h3>
-                <a href={`http://localhost:3000/google?providerId=${providerId}`}>Login with Google</a>
+                <a href={`${googleAuthUrl}?providerId=${providerId}`}>Login with Google</a>
             </div>
         </div>
-    )
+    );
 }
 
 export default Auth;
