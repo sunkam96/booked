1a1d27ba31de9f29f158e21f6526b222e6192a08
sunkam96 <sunkam.sai@gmail.com>
2025-09-20 19:35:32 -0400

    initial v2 card structure

--- src/v2/common/ServiceItemCard.tsx
+++ src/v2/common/ServiceItemCard.tsx
@@ -1,12 +1,41 @@
-import './Common.css';
+import './ServiceItemCard.css';
 
 function ServiceItemCard() {
+    const handleClick = () => {
+        console.log('Service item clicked!');
+    };
+
+    const handleKeyDown = (event: React.KeyboardEvent) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.preventDefault();
+            handleClick();
+        }
+    };
+
     return (
-        <div className="service-item-card-container">
+        <div 
+            className="service-item-card-container"
+            onClick={handleClick}
+            onKeyDown={handleKeyDown}
+            tabIndex={0}
+            role="button"
+            aria-label="Select service"
+        >
             <div className="service-item-logo"></div>
-            <div className="service-item-content"></div>
+            <div className="service-item-content">
+                <div className="service-item-content-pricing">
+                    <div className="service-item-content-price-typography">$50</div>
+                    <div className="service-item-content-duration-typography">(1hr)</div>
+                </div>
+                <div className="service-item-content-divider"></div>
+                <div className="service-item-content-description">
+                    <div className="service-item-content-description-provider-name-typography">David T</div>
+                    <div className="service-item-content-description-service-name-typography">Haircut and Beard Trim</div>
+                    <div className="service-item-content-description-service-details-typography">Greenpoint, Brooklyn</div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default ServiceItemCard;
